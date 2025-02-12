9aa842b400944d579fd2137a84b7d4f4b567a0bd
Tim <Timmvanc@gmail.com>
2025-02-12 17:24:58 -0500

    Fix missing useEffect import in Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../axiosInstance';
 import logo from '../assets/images/artalyze-logo.png';

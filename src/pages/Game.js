8138f383c5e3d9c7aea0f3199c4bb2b97c045d7f
Tim <Timmvanc@gmail.com>
2025-02-13 11:19:37 -0500

    Increase double tap timing to 200ms

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1095,7 +1095,7 @@ const Game = () => {
                             } else {
                               singleTapTimeout.current = setTimeout(() => {
                                 handleSelection(image, image === pair.human); // ✅ Select only if no double-tap
-                              }, 150);
+                              }, 200);
                             }
 
                             lastTapTime.current = currentTime;
